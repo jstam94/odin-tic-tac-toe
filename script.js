6af727a5f46908222833c4deaf38ba3df48ffe4e
Jordan Stammer <s.jordanstammer@gmail.com>
2023-11-28 18:50:49 -0500

    Refactor PlayerSelection Function
    Ensured Player Selection Function is within game object

--- script.js
+++ script.js
@@ -1,18 +1,15 @@
-function createPlayer(name, marker){
-        return {name, marker};
+let ticTacToe ={
+    players: [],
+    playerSelection : function(){
+        function createPlayer(name, marker){
+            return {name, marker};
+        }
+        let playerOneName = 'playerX';
+        let markerChoice = 'X';
+        let playerTwoName = 'playerO'
+        let remainingMarker;
+        markerChoice === 'X'? remainingMarker = 'O': remainingMarker = 'O';
+        this.players.push(createPlayer(playerOneName, markerChoice));
+        this.players.push(createPlayer(playerTwoName, remainingMarker));
     }
-
-let players = [];
-
-function playerSelection(){
-    let playerOneName = 'playerX';
-    let markerChoice = 'X';
-    let playerTwoName = 'playerO'
-    let remainingMarker;
-    markerChoice === 'X'? remainingMarker = 'O': remainingMarker = 'O';
-    players.push(createPlayer(playerOneName, markerChoice));
-    players.push(createPlayer(playerTwoName, remainingMarker));
 }
-
-
-
