81d0d1e4954f23282d626d13d1680ba99971c95d
Jordan Stammer <s.jordanstammer@gmail.com>
2023-11-28 18:44:05 -0500

    Add PlayerSelection Function
    Functionality creates player object and adds to players array

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
- function createPlayer(name, marker){
-    return {name, marker};
- }
+function createPlayer(name, marker){
+        return {name, marker};
+    }
+
+let players = [];
+
+function playerSelection(){
+    let playerOneName = 'playerX';
+    let markerChoice = 'X';
+    let playerTwoName = 'playerO'
+    let remainingMarker;
+    markerChoice === 'X'? remainingMarker = 'O': remainingMarker = 'O';
+    players.push(createPlayer(playerOneName, markerChoice));
+    players.push(createPlayer(playerTwoName, remainingMarker));
+}
+
+
+
