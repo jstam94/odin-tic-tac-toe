6d280e89659aaf93a4cee841061fc11c0343c63f
Jordan Stammer <s.jordanstammer@gmail.com>
2023-12-01 00:32:17 -0500

    s

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// let players ={
-//     players: [],
-//     playerSelection : function(){
-//         function createPlayer(name, marker){
-//             return {name, marker};
-//         }
-//         let playerOneName = 'playerX';
-//         let markerChoice = 'X';
-//         let playerTwoName = 'playerO'
-//         let remainingMarker;
-//         markerChoice === 'X'? remainingMarker = 'O': remainingMarker = 'O';
-//         this.players.push(createPlayer(playerOneName, markerChoice));
-//         this.players.push(createPlayer(playerTwoName, remainingMarker));
-//     }
-// }
-
 
 let players = (function(){
     let players = [];
