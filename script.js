8e6bc6af09a645cc1e81709365924122a0e9a570
Jordan Stammer <s.jordanstammer@gmail.com>
2023-11-30 23:47:55 -0500

    Build Gameboard Function
    Used Module Pattern as there will only be a single gambeboard

--- script.js
+++ script.js
@@ -25,3 +25,27 @@ function cell() {
         getValue
 }
 }
+
+
+
+gameBoard = (function(){
+    const rows = 3;
+    const columns = 3;
+    const board = [];
+
+    for (let i = 0; i < rows; i++) {
+        board[i] = [];
+        for (let j = 0; j < columns; j++) {
+          board[i].push(cell());
+        }
+      }
+      const getBoard = () => board;
+      const markHere = (position) => {
+        position.markHere();
+      }
+      const printBoard = () => {
+        const boardWithCellValues = board.map((row) => row.map((cell) => cell.getValue()))
+        console.log(boardWithCellValues);
+      };
+      return { getBoard, markHere, printBoard }
+})();
